8bc01990291c03bf243ace35424d1e299741ec27
Shuo <shuoyang1213@hotmail.com>
2022-03-03 19:59:30 +1100

    update backend address

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -80,7 +80,7 @@ const Recorder = () => {
     })
     micStream.setStream(userMediaStream)
 
-    const preSignedURL = await axios.post('https://transcribe-app-alb-1970972784.ap-southeast-2.elb.amazonaws.com/api/transcribe', {
+    const preSignedURL = await axios.post('https://api.transcribestreamingapp.store/', {
       sampleR: sampleRate
     })
     console.log(preSignedURL.data)
